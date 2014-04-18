33ccac9d2d1d2c4f612e6ea74e2c30b436f9f800
yangyu0116 <yangyu@sina.cn>
2014-04-18 22:34:26 +0800

    update

--- scrapy/nbavideo/nbavideo/spiders/nbavideo_spider.py
+++ scrapy/nbavideo/nbavideo/spiders/nbavideo_spider.py
@@ -8,7 +8,7 @@ class NbavideoSpider(BaseSpider):
     name = "nbavideo"
     allowed_domains = ["sina.com","sina.com.cn"]
     start_urls = [
-        "http://api.roll.news.sina.com.cn/zt_list?channel=sports&cat_3=video&cat_1=lq-nba&tag=1&show_ext=1&show_all=1&show_cat=1&format=json&show_num=20",
+        "http://api.roll.news.sina.com.cn/zt_list?channel=sports&cat_3=video&cat_1=lq-nba&tag=1&show_ext=1&show_all=1&show_cat=1&format=json&show_num=3000",
     ]
 
     def parse(self, response):
@@ -20,28 +20,24 @@ class NbavideoSpider(BaseSpider):
         items = []
         for video in video_box:
 			item = NbavideoItem()
-			item['title'] = video['title']
-			item['type'] = video['cat_4_name']
+			item['title'] = video['title'].encode('utf8')
+			item['type'] = video['cat_4_name'].encode('utf8')
+			if  '视频集锦' in item['title'] or '视频录播' in item['title']:
+			    item['title'] =  item['title'].replace('视频','比赛')
+			elif item['type'] == '官方' or item['type'] == '节目' or item['type'] == '策划' or item['type'] == '访谈' or item['type'] == '花絮':
+			    item['title'] = item['type']+'-'+item['title']
+			elif item['type'] == '集锦':
+			    item['title'] = '比赛集锦-'+item['title']
+			else:
+			    item['title'] =  item['title'][item['title'].find('-') + 1:]
 			item['url'] = video['url']
 			item['urlmd5'] = hashlib.md5(item['url']).hexdigest().lower()
 			item['pic'] = video['img']
 			item['createtime'] = video['createtime']
 			item['addtime'] = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(time.time()))
 			item['site'] = 'sina.com.cn'
-#			if  in item['title'] or '视频录播' in item['title']:
-#			    item['title'] =  item['title'].replace('视频','比赛')
-#			else:
-#			    item['title'] =  item['title'][item['title'].find('-') + 1:]
-#			item['url'] = main_info.select('a/@href').extract()[0]
-#			item['urlmd5'] = hashlib.md5(item['url']).hexdigest().lower()
-#			item['pic'] = main_info.select('a/img/@src').extract()[0]
-#			
-#			item['vyear'] = time.strftime("%Y", time.localtime(time.time()))
-#			item['vdate'] = video.select('div[@class="c_txt"]/div[@class="c_info"]/text()').extract()[0]
-#			item['vdate'] = item['vdate'][0:+item['vdate'].find(' ')]
 
 			items.append(item)
-#
         items.reverse()
         return items
 
